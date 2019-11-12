ab923f9c0c77a322db290260cd7b845c7598dc80
Jason Dixon <jason.dixon.email@gmail.com>
2019-11-13 08:42:56 +1300

    include touch event

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 var page = document.getElementById("body");
 page.addEventListener("mousedown", function(event){
+  event.preventDefault();
   beep.play();
   page.classList.add("flash");
 });
+page.addEventListener("touchstart", function(event){
+  event.preventDefault();
+  beep.play();
+  page.classList.add("flash");
+});
+
 
 var beep = new Audio("audio/beep.mp3");
 beep.load();
