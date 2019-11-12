7ddfa8c718859132aa7bf9e3a72be5c3fda80e1d
Jason Dixon <jason.dixon.email@gmail.com>
2019-11-12 22:15:57 +1300

    cleaned it up

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 
+beep = new Audio("audio/beep.mp3");
+beep.load();
+
 window.onload = function(){
-  var scanner = document.getElementById("scanner");
-  var beep = new Audio("https://freesound.org/data/previews/144/144418_62484-lq.mp3")
-  scanner.onclick = function() {
+  var page = document.getElementById("body");
+  page.onclick = function() {
     beep.play();
   }
 }
